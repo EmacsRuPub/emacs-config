36ffcff918349ea43905f53c725bc31c31969b2f
Ronin <aaermolov@gmail.com>
2011-03-24 00:43:05 +0300

    Core: path constants refactored (added base path refs)

--- constants.el
+++ constants.el
@@ -7,13 +7,14 @@
 ;; Requirements:
 ;; Status: not intended to be distributed yet
 
+(setq custom/base-conf-dir "~/.emacs.d")
+(setq custom/base-ext-dir "~/site-lisp")
+
 ;#############################################################################
 ;#   Abbreviation
 ;############################################################################
-(setq custom/yasnippet-dir "~/.emacs.d/resources/yasnippet")
-(setq custom/msf-abbrev-dir "~/.emacs.d/resources/msf-abbrevs")
-(setq custom/auto-insert-dir (concat (getenv "HOME") "/.emacs.d/resources/auto-insert/"))
-;; (setq custom/company-mode-dir "~/emacs/company-mode") ;; FIXME value
+(setq custom/yasnippet-dir (concat custom/base-conf-dir "/resources/yasnippet"))
+(setq custom/auto-insert-dir (concat custom/base-conf-dir "/resources/auto-insert/"))
 
 
 ;#############################################################################
@@ -34,34 +35,34 @@
 ;#   Extension paths
 ;############################################################################
 ;; Google weather
-(setq custom-ext-path/gweather "~/site-lisp/clients/google-weather.el")
-(setq custom-ext-path/org-gweather "~/site-lisp/clients/org-google-weather.el")
+(setq custom-ext-path/gweather (concat custom/base-ext-dir "/clients/google-weather.el"))
+(setq custom-ext-path/org-gweather (concat custom/base-ext-dir "/clients/org-google-weather.el"))
 ;; DVC
-(setq custom-ext-path/dvc-dir "~/site-lisp/vcs/dvc/")
-(setq custom-ext-path/dvc-load "~/site-lisp/vcs/dvc/dvc-load.el")
+(setq custom-ext-path/dvc-dir (concat custom/base-ext-dir "/vcs/dvc/"))
+(setq custom-ext-path/dvc-load (concat custom/base-ext-dir "/vcs/dvc/dvc-load.el"))
 ;; Git
-(setq custom-ext-path/git "~/site-lisp/vcs/git.el")
-(setq custom-ext-path/egit "~/site-lisp/vcs/egit.el")
-(setq custom-ext-path/gitsum "~/site-lisp/vcs/gitsum.el")
-(setq custom-ext-path/egg "~/site-lisp/vcs/egg")
-;; (setq custom-ext-path/git-emacs "~/site-lisp/vcs/git-emacs")
+(setq custom-ext-path/git (concat custom/base-ext-dir "/vcs/git.el"))
+(setq custom-ext-path/egit (concat custom/base-ext-dir "/vcs/egit.el"))
+(setq custom-ext-path/gitsum (concat custom/base-ext-dir "/vcs/gitsum.el"))
+(setq custom-ext-path/egg (concat custom/base-ext-dir "/vcs/egg"))
+;; (setq custom-ext-path/git-emacs (concat custom/base-ext-dir "/vcs/git-emacs"))
 ;; Vcs
-(setq custom-ext-path/vcs "~/site-lisp/vcs")
+(setq custom-ext-path/vcs (concat custom/base-ext-dir "/vcs"))
 ;; lisp
-;; (setq custom-ext-path/slime "~/site-lisp/slime/")
+;; (setq custom-ext-path/slime (concat custom/base-ext-dir "/slime/"))
 ;; nxml
-(setq custom-ext-path/docbook-menu "~/site-lisp/publishing/docbook-menu")
-(setq custom-ext-path/nxml-mode "~/site-lisp/nxml-mode")
-(setq custom-ext-path/rng-auto "~/site-lisp/nxml-mode/rng-auto.el")
+(setq custom-ext-path/docbook-menu (concat custom/base-ext-dir "/publishing/docbook-menu"))
+(setq custom-ext-path/nxml-mode (concat custom/base-ext-dir "/nxml-mode"))
+(setq custom-ext-path/rng-auto (concat custom/base-ext-dir "/nxml-mode/rng-auto.el"))
 ;; ccmode
-(setq custom-ext-path/c-eldoc "~/site-lisp/programming/c-eldoc.el")
+(setq custom-ext-path/c-eldoc (concat custom/base-ext-dir "/programming/c-eldoc.el"))
 ;; haskell
-(setq custom-ext-path/shime "~/site-lisp/shime")
+(setq custom-ext-path/shime (concat custom/base-ext-dir "/shime"))
 ;; javascript
 ;; (setq custom-ext-path/ejacs "~/emacs/ejacs")
 ;; jabber
-(setq custom-ext-path/autosmiley "~/site-lisp/clients/autosmiley.el")
+(setq custom-ext-path/autosmiley (concat custom/base-ext-dir "/clients/autosmiley.el"))
 ;; project-buffer-mode
-(setq custom-ext-path/project-buffer-mode "~/site-lisp/projects")
+(setq custom-ext-path/project-buffer-mode (concat custom/base-ext-dir "/projects"))
 
 ;;; constants.el ends here
