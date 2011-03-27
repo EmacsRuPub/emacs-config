9a023461e2ac997982dc550d225b2c10a8120eaa
Ronin <aaermolov@gmail.com>
2011-03-28 03:15:37 +0400

    Access: removed popup-select window, it is not so useful

--- rc/access/access.el
+++ rc/access/access.el
@@ -11,6 +11,5 @@
 (load "~/.emacs.d/rc/access/rc-bookmarks.el")
 (load "~/.emacs.d/rc/access/rc-icicles.el")
 (load "~/.emacs.d/rc/access/rc-search.el")
-(load "~/.emacs.d/rc/access/rc-popup-test.el")
 
 ;;; access.el ends here
