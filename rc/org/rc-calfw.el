2700cb424561d489757a66ea6d01161fbd1c2f71
Ronin <aaermolov@gmail.com>
2011-09-22 18:15:48 +0400

    Org: Added rather handy calendar frontend

---
+++ rc/org/rc-calfw.el
@@ -0,0 +1,21 @@
+;;; rc-calfw.el ---
+
+;; Copyright (C) 2011 Alex Ermolov
+;;
+;; Author: aaermolov@gmail.com
+;; Keywords:
+;; Requirements:
+;; Status: not intended to be distributed yet
+
+;#############################################################################
+;#   Load extensions
+;############################################################################
+(require 'calfw)
+
+
+;#############################################################################
+;#   Keybindings
+;############################################################################
+(global-set-key "\C-cc" 'cfw:open-calendar-buffer)
+
+;;; rc-calfw.el ends here
