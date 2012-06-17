61b430cc978c877daed9c0fc103ebafd23ab16cb
Octocat <aaermolov@gmail.com>
2012-06-17 15:13:36 +0400

    Twitter seems to work

--- rc/extras/client/rc-twitter.el
+++ rc/extras/client/rc-twitter.el
@@ -1,12 +1,14 @@
-;;; rc-twitter.el --- 
+;;; rc-twitter.el ---
 
 ;; Copyright (C) 2011 Alex Ermolov
 ;;
 ;; Author: aaermolov@gmail.com
-;; Keywords: 
-;; Requirements: 
+;; Keywords:
+;; Requirements:
 ;; Status: not intended to be distributed yet
 
-(setq twittering-use-master-password t)
+(require 'twittering-mode)
+
+;; (setq twittering-use-master-password t)
 
 ;;; rc-twitter.el ends here
