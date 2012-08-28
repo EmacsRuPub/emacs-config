5c7d9e8066b0ad4ecdfd23708454c88dfae47ed6
Octocat <aaermolov@gmail.com>
2012-08-28 21:01:25 +0400

    Refactored init.el

--- init.el
+++ init.el
@@ -4,25 +4,26 @@
     (error (display-warning 'initialization (concat "Load of " filename " failed "
                                                     (prin1-to-string err)) :warning))))
 
-(setq config-basedir "/Users/octocat/.emacs.d")
 (setq message-log-max t)
 
-(setq el-get-sources-system-file
-  (concat config-basedir "/el-get-sources-system.el"))
+(setq config-basedir "/Users/octocat/.emacs.d")
 
-(solidload (concat config-basedir "/rc-el-get.el"))
-(solidload (concat config-basedir "/ext/load-ext-manual.el"))
-(solidload (concat config-basedir "/defuns/defuns.el"))
 (setq custom-file (concat config-basedir "/customizations.el"))
-(solidload (concat config-basedir "/constants.el"))
-(solidload (concat config-basedir "/credentials.el"))
-
-(solidload (concat config-basedir "/emacs-rc-auto-mode.el"))
 
 (let ((default-directory (concat config-basedir "/rc")))
   (normal-top-level-add-subdirs-to-load-path))
-(solidload (concat config-basedir "/bundle.el"))
 
-(solidload (concat config-basedir "/rc-desktop.el"))
+(mapcar 'solidload
+        (mapcar
+         (lambda (path) (concat config-basedir path))
+         '("/rc-el-get.el"
+           "/ext/load-ext-manual.el"
+           "/defuns/defuns.el"
+           "/constants.el"
+           "/credentials.el"
+           "/bundle.el"
+           "/emacs-rc-auto-mode.el"
+           "/rc-desktop.el")
+         ))
 
 (load custom-file)
