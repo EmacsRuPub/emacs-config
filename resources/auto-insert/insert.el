2939f18aac3e0ba5de0bc99c0a8aa94ad417b692
Octocat <aaermolov@gmail.com>
2012-09-27 00:48:35 +0400

    Updated auto-insert template for elisp sources

--- resources/auto-insert/insert.el
+++ resources/auto-insert/insert.el
@@ -1,13 +1,10 @@
-;;; (>>FILE<<) --- 
+;;; (>>FILE<<) ---
 
-;; Copyright (C) 2011 Alex Ermolov
+;; Copyright (C) 2012 Alex Ermolov
 ;;
 ;; Author: aaermolov@gmail.com
-;; Keywords: 
-;; Requirements: 
-;; Status: not intended to be distributed yet
-
-
+;; Keywords:
+;; Requirements:
 
 
 ;;; (>>FILE<<) ends here
