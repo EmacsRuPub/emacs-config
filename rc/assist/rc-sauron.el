c71b70bc7a5486a3349e43191e7bb56c4135b3ef
Octocat <oc7oca7@gmail.com>
2012-11-27 01:48:29 +0400

    Sauron config update

--- rc/assist/rc-sauron.el
+++ rc/assist/rc-sauron.el
@@ -9,8 +9,29 @@
 
 (require 'sauron)
 
+;; note, you add (setq sauron-debug t) to get errors which can debug if
+;; there's something wrong; normally, we catch such errors, since e.g an error
+;; in one of the hooks may cause ERC to fail (i.e., the message won't come
+;; trough).
+
 (setq sauron-separate-frame nil)
 (setq sauron-hide-mode-line t)
+(setq sauron-max-line-length 120)
+;; (setq sauron-watch-patterns
+;;       '("emacs-fu" "emacsfu" "wombat" "capybara" "yak" "gnu" "\\bmu\\b"))
+;; (setq sauron-watch-nicks
+;;       '("Tom" "Dick" "Harry"))
+
+(global-set-key (kbd "C-c s s") 'sauron-start)
+(global-set-key (kbd "C-c s p") 'sauron-start)
+(global-set-key (kbd "C-c s t") 'sauron-toggle-hide-show)
+(global-set-key (kbd "C-c s c") 'sauron-clear)
+
+;; ;; events to ignore
+;; (add-hook 'sauron-event-block-functions
+;;   (lambda (origin prio msg &optional props)
+;;     (or
+;;       (string-match "^*** Users" msg)))) ;; filter out IRC spam
 
 (provide 'rc-sauron)
 
