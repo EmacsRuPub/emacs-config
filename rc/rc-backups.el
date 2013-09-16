55ae3690ca559868c97904209735e66914349613
Octocat <oc7oca7@gmail.com>
2013-09-17 01:47:18 +0400

    Fixed backups config

--- rc/rc-backups.el
+++ rc/rc-backups.el
@@ -7,16 +7,16 @@
 ;; Requirements:
 ;; Status: not intended to be distributed yet
 
-(setq backup-by-copying t              ; don't clobber symlinks
-      backup-by-copying-when-linked t  ; Copy linked files, don't rename.
-      backup-directory-alist
-      '(("." . "~/.emacs-backups"))    ; don't litter my fs tree
-      auto-save-file-name-transforms nil
-      delete-old-versions t
-      kept-new-versions 16
-      kept-old-versions 2
-      version-control t)               ; use versioned backups
+(defconst emacs-tmp-dir "~/.emacs-backups")
+
+(setq backup-by-copying t)              ; don't clobber symlinks
+(setq backup-by-copying-when-linked t)  ; Copy linked files, don't rename.
+(setq backup-directory-alist `(("." . ,emacs-tmp-dir)))    ; don't litter my fs tree
+(setq auto-save-file-name-transforms `((".*" ,emacs-tmp-dir t)))
+(setq delete-old-versions t)
+(setq kept-new-versions 16)
+(setq kept-old-versions 2)
+(setq version-control t)               ; use versioned backups
 
 (provide 'rc-backups)
-
 ;;; rc-backups.el ends here
