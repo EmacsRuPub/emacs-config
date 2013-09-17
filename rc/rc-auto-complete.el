67c9d222fb33b35deea69d31d3653a2cb21327b3
Octocat <oc7oca7@gmail.com>
2013-09-17 23:03:18 +0400

    Auto-complete updates: slime, nrepl

--- rc/rc-auto-complete.el
+++ rc/rc-auto-complete.el
@@ -8,6 +8,7 @@
 
 (require 'auto-complete)
 (require 'auto-complete-config)
+(require 'ac-nrepl)
 
 ;; (add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
 (ac-config-default)
@@ -16,10 +17,19 @@
 ;; add lisp autocomplete-support
 (require 'ac-slime)
 
+(defun set-auto-complete-as-completion-at-point-function ()
+  (setq completion-at-point-functions '(auto-complete)))
+(add-hook 'auto-complete-mode-hook 'set-auto-complete-as-completion-at-point-function)
+
 (add-hook 'slime-mode-hook 'set-up-slime-ac)
 (add-hook 'slime-repl-mode-hook 'set-up-slime-ac)
-(eval-after-load "auto-complete"
-  '(add-to-list 'ac-modes 'slime-repl-mode))
+(eval-after-load "auto-complete" '(add-to-list 'ac-modes 'slime-repl-mode))
+
+(add-hook 'nrepl-mode-hook 'ac-nrepl-setup)
+(add-hook 'nrepl-interaction-mode-hook 'ac-nrepl-setup)
+(add-hook 'nrepl-mode-hook 'set-auto-complete-as-completion-at-point-function)
+(add-hook 'nrepl-interaction-mode-hook 'set-auto-complete-as-completion-at-point-function)
+(eval-after-load "auto-complete" '(add-to-list 'ac-modes 'nrepl-mode))
 
 (provide 'rc-auto-complete)
 
