9747de6fb95769b61d864bbabd1af916d391e20c
Octocat <oc7oca7@gmail.com>
2013-09-17 23:17:29 +0400

    Auto-complete for php

--- rc/rc-lang-php.el
+++ rc/rc-lang-php.el
@@ -9,6 +9,9 @@
 
 (require 'php-mode) ;;подгружаем php режим
 (require 'php-electric) ;;режим autocompletion конструкций языка
+(require 'ac-php-doc)
+
+(setq php-manual-path "/home/octocat/documentation/php-chunked-xhtml/")
 
 (defun custom/php-mode-hook ()
   ;; style customization
@@ -17,7 +20,9 @@
   (setq c-basic-offset 4)
   (setq show-trailing-whitespace t) ;; show trailing whitespace
 )
+
 (add-hook 'php-mode-hook 'custom/php-mode-hook)
+(add-hook 'php-mode-hook 'set-up-php-ac)
 
 (provide 'rc-lang-php)
 
