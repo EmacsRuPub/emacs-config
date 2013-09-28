8eba5884dc51a52a5c24ddb34821dff44059bd44
Octocat <oc7oca7@gmail.com>
2013-09-29 00:16:26 +0400

    Changed prefix key for opening portage files

--- rc/rc-edit-portage.el
+++ rc/rc-edit-portage.el
@@ -7,12 +7,12 @@
 ;; Requirements:
 ;; Status: not intended to be distributed yet
 
-(global-set-key (kbd "\C-c p c") (lambda () (interactive) (find-file "/sudo::/etc/portage/make.conf")))
-(global-set-key (kbd "\C-c p m") (lambda () (interactive) (find-file "/sudo::/etc/portage/package.mask")))
-(global-set-key (kbd "\C-c p u") (lambda () (interactive) (find-file "/sudo::/etc/portage/package.use")))
-(global-set-key (kbd "\C-c p l") (lambda () (interactive) (find-file "/sudo::/etc/portage/package.license")))
-(global-set-key (kbd "\C-c p k") (lambda () (interactive) (find-file "/sudo::/etc/portage/package.keywords")))
-(global-set-key (kbd "\C-c p a") (lambda () (interactive) (find-file "/sudo::/etc/portage/package.accept_keywords")))
+(global-set-key (kbd "\C-c g c") (lambda () (interactive) (find-file "/sudo::/etc/portage/make.conf")))
+(global-set-key (kbd "\C-c g m") (lambda () (interactive) (find-file "/sudo::/etc/portage/package.mask")))
+(global-set-key (kbd "\C-c g u") (lambda () (interactive) (find-file "/sudo::/etc/portage/package.use")))
+(global-set-key (kbd "\C-c g l") (lambda () (interactive) (find-file "/sudo::/etc/portage/package.license")))
+(global-set-key (kbd "\C-c g k") (lambda () (interactive) (find-file "/sudo::/etc/portage/package.keywords")))
+(global-set-key (kbd "\C-c g a") (lambda () (interactive) (find-file "/sudo::/etc/portage/package.accept_keywords")))
 
 (provide 'rc-edit-portage)
 
