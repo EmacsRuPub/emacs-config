6a38f189832ef4abcb24ccaa03b4822e87920523
Octocat <oc7oca7@gmail.com>
2013-12-11 18:05:56 +0400

    Enable projectile-find-file back

--- rc/rc-search.el
+++ rc/rc-search.el
@@ -41,7 +41,7 @@
 (define-key custom-search-keymap (kbd "g") 'projectile-ag)
 (define-key custom-search-keymap (kbd "p") 'ag-regexp-project-at-point)
 (define-key custom-search-keymap (kbd "i") 'helm-swoop) ;; was ioccur
-(define-key custom-search-keymap (kbd "q") 'ag-find-file)
+(define-key custom-search-keymap (kbd "q") 'projectile-find-file)
 (define-key custom-search-keymap (kbd "s") 'ido-goto-symbol)
 (define-key custom-search-keymap (kbd "8") 'ace-jump-char-mode)
 (define-key custom-search-keymap (kbd "9") 'ace-jump-word-mode)
