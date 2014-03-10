0bb1054213eee99d7fd2884532a327f01cf89272
Alex <aaermolov@gmail.com>
2014-03-10 17:28:48 +0400

    updated magit keybindings according to subjective usefulness

--- rc/rc-vcs-git.el
+++ rc/rc-vcs-git.el
@@ -7,12 +7,10 @@
 
 (global-git-gutter-mode +1)
 
-(global-set-key (kbd "C-c g s") 'magit-status)
-(global-set-key (kbd "C-c g l") 'magit-log)
-(global-set-key (kbd "C-c g c") 'magit-file-log)
 (global-set-key (kbd "C-c C-g") 'magit-blame-mode)
+(global-set-key (kbd "C-c g s") 'magit-status)
+(global-set-key (kbd "C-c g l") 'magit-file-log)
 (global-set-key (kbd "C-c g w") 'magit-diff-working-tree)
-(global-set-key (kbd "C-c g f") 'magit-fetch-current)
-(global-set-key (kbd "C-c g r") 'magit-rebase-step)
+(global-set-key (kbd "C-c g r") 'magit-reflog)
 
 (provide 'rc-vcs-git)
