803f8e0919c6c30540fcd59e037386e377334ead
Alex <aaermolov@gmail.com>
2014-06-01 20:53:34 +0400

    rc: extracted completion custom defs

---
+++ customdefs/custom-completion.el
@@ -0,0 +1,105 @@
+;; -*- coding: utf-8 -*-
+;;
+;; Filename: custom-yasnippet.el
+;; Created: Вс июн  1 20:49:09 2014 (+0400)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun ac-yasnippet-candidate ()
+  (let ((table (yas/get-snippet-tables major-mode)))
+    (if table
+      (let (candidates (list))
+            (mapcar (lambda (mode)
+              (maphash (lambda (key value)
+                (push key candidates))
+              (yas/snippet-table-hash mode)))
+            table)
+        (all-completions ac-prefix candidates)))))
+
+(defun ac-yasnippet-candidate-1 (table)
+  (let ((hashtab (yas/snippet-table-hash table))
+        (parent (if (fboundp 'yas/snippet-table-parent)
+                    (car (yas/snippet-table-parent table))))
+        candidates)
+    (maphash (lambda (key value)
+               (push key candidates))
+             hashtab)
+    (setq candidates (all-completions ac-prefix (nreverse candidates)))
+    (if parent
+        (setq candidates
+              (append candidates (ac-yasnippet-candidate-1 parent))))
+    candidates))
+
+(defface ac-yasnippet-candidate-face
+  '((t (:background "sandybrown" :foreground "black")))
+  "Face for yasnippet candidate.")
+
+(defface ac-yasnippet-selection-face
+  '((t (:background "coral3" :foreground "white")))
+  "Face for the yasnippet selected candidate.")
+
+(defvar ac-source-yasnippet
+  '((candidates . ac-yasnippet-candidate)
+    (action . yas/expand)
+    (limit . 3)
+    (candidate-face . ac-yasnippet-candidate-face)
+    (selection-face . ac-yasnippet-selection-face))
+  "Source for Yasnippet.")
+
+(defun set-auto-complete-as-completion-at-point-function ()
+  (setq completion-at-point-functions '(auto-complete)))
+
+;; Inter-field navigation
+(defun yas/goto-end-of-active-field ()
+  (interactive)
+  (let* ((snippet (car (yas/snippets-at-point)))
+         (position (yas/field-end (yas/snippet-active-field snippet))))
+    (if (= (point) position)
+        (move-end-of-line)
+      (goto-char position))))
+
+(defun yas/goto-start-of-active-field ()
+  (interactive)
+  (let* ((snippet (car (yas/snippets-at-point)))
+         (position (yas/field-start (yas/snippet-active-field snippet))))
+    (if (= (point) position)
+        (move-beginning-of-line)
+      (goto-char position))))
+
+(defun yas-popup-isearch-prompt (prompt choices &optional display-fn)
+  (when (featurep 'popup)
+    (popup-menu*
+     (mapcar
+      (lambda (choice)
+        (popup-make-item
+         (or (and display-fn (funcall display-fn choice))
+             choice)
+         :value choice))
+      choices)
+     :prompt prompt
+     :isearch t
+     )))
+
+(defun yas-safer-expand ()
+  (let ((yas/fallback-behavior 'return-nil))
+    (call-interactively 'yas/expand)))
+
+(defun yas-ido-insert-snippets (&optional no-condition)
+  (interactive "P")
+  (let ((yas-prompt-functions '(yas/ido-prompt)))
+    (yas/insert-snippet)))
+
+(defun yasnippet-unbind-trigger-key ()
+  "Unbind `yas/trigger-key'."
+  (let ((key yas/trigger-key))
+    (setq yas/trigger-key nil)
+    (yas/trigger-key-reload key)))
+
+;; hook for automatic reloading of changed snippets
+(defun update-yasnippets-on-save ()
+  (when (string-match "/resources/yasnippet" buffer-file-name)
+    (yas/load-directory custom/yasnippet-dir)))
+
+(provide 'custom-completion)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; custom-yasnippet.el ends here
