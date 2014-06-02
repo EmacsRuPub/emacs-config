6d2a916f88e104b09b0418bd2a43ea4788596b6b
Alex <aaermolov@gmail.com>
2014-06-02 16:08:19 +0400

    resources: removed actually unused jabber smileys

--- customdefs/custom-jabber.el
+++ customdefs/custom-jabber.el
@@ -50,66 +50,6 @@
                  (s-join "\n"
                          (mapcar (lambda (x) (s-prepend "> " x)) result)))))))
 
-
-;#############################################################################
-;#   Jabber smileys
-;############################################################################
-(require 'xml)
-
-(defun smiley-parse-node (node)
-  "Extracts smiley data of type (REGEXP 1 FILENAME) from a node."
-  (when (listp node)
-    (let* ((filename
-            (cdr (assq 'file (xml-node-attributes node))))
-           (smilies
-            (mapcar
-             (lambda (data) (car (last data)))
-             (xml-get-children node 'string))))
-                        (list
-        (concat (regexp-opt smilies "\\(") "\\W")
-        1 filename)
-       )))
-
-(defun smiley-parse-file (filename)
-  "Returns smiley data list for smiley-regexp-alist."
-  (let ((root (xml-parse-file filename)))
-    (mapcar 'smiley-parse-node
-            (xml-get-children
-             (car root) ;; we skip <messaging-emoticon-map>
-             'emoticon))
-    ))
-
-(defvar smiley-base-directory nil
-  "Directory containing smiley packs (folders).")
-
-(defun smiley-load-theme (pack)
-  "Loads smiley theme PACK into smiley.el.
-  Each theme should be a folder inside smiley-data-directory. For example
-  smiley-data-directory is set to '/home/bobry/.emacs.d/smileys, so possible
-  directory structure could be:
-  .
-  |-- default
-  |-- kolobok
-  `-- tango
-
-  default, kolobok and tango are valid PACK values."
-  (interactive "sTheme: ")
-  (when smiley-base-directory
-    (let* ((smiley-dir (format "%s/%s/" smiley-base-directory pack ))
-                                         (smiley-path (concat smiley-dir "emoticons.xml")))
-      (when (file-exists-p smiley-path)
-        (setq smiley-style 'low-color) ;; this is done for compatibility reasons
-        (setq smiley-data-directory smiley-dir)
-        (setq smiley-regexp-alist (smiley-parse-file smiley-path))
-        (smiley-update-cache))
-      )))
-
-;; in client code:
-;; (add-hook 'jabber-chat-mode-hook 'autosmiley-mode)
-;; (setq smiley-base-directory (concat (getenv "HOME") "/.emacs.d/resources/smileys/"))
-;; (add-to-list 'gnus-smiley-file-types "gif")
-;; (smiley-load-theme "kolobok")
-
 (provide 'custom-jabber)
 
 ;;; defun-jabber-custom.el ends here
