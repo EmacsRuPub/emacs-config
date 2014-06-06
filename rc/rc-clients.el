8b4d5b2fc2b2145b355d8fa0191b1de34fcd6869
Alex <aaermolov@gmail.com>
2014-06-06 13:37:51 +0400

    rc: moved ERC customizations to inventory temporarily
    it was done to figure out connection problems, so some options
    will be back in the future

--- rc/rc-clients.el
+++ rc/rc-clients.el
@@ -7,76 +7,14 @@
 (autoload 'twit "twittering-mode" nil t)
 (autoload 'mingus "mingus" nil t)
 
-(eval-after-load "erc"
-  '(progn
-     (require 'erc-join)
-     (require 'erc-lang)
-     (require 'erc-fill)
-     (require 'erc-log)
-     (require 'erc-autoaway)
-     (require 'erc-services)
-     (require 'erc-menu)
-     (require 'erc-ring)
-     (require 'erc-match)
 
-     (add-to-list 'erc-modules 'match)
-     (erc-update-modules)
-     (erc-autojoin-mode t)
-     (erc-fill-mode t)
-     (erc-nickserv-mode 1)
-     (erc-ring-mode t)
-     (erc-match-enable)
-     (erc-match-mode 1)
-     (erc-timestamp-mode t)
-     (erc-log-enable)
-
-     (setq erc-user-full-name custom/erc-full-name)
-     (setq erc-email-userid custom/gmail-address)
-     (setq erc-log-insert-log-on-open nil)
-     (setq erc-log-channels t)
-     (setq erc-log-channels-directory custom/erc-logs-directory)
-     (setq erc-save-buffer-on-part t)
-     (setq erc-hide-timestamps nil)
-     (setq erc-max-buffer-size 20000)
-     (setq erc-autoaway-idle-seconds 1200)
-     (setq erc-autoaway-message custom/erc-autoaway-message)
-     (setq erc-auto-discard-away t)
-     (setq erc-auto-query 'buffer)
-     (setq erc-track-exclude-types '("JOIN" "NICK" "PART" "QUIT"))
-     (setq erc-prompt-for-nickserv-password nil)
-     (setq erc-nickserv-passwords
-           '((freenode     ((custom/erc-nick . custom/erc-nickserv-password)
-                            (custom/erc-nick . custom/erc-nickserv-password)))))
-     (setq erc-encoding-coding-alist
-           '(("#debian-russian" . cyrillic-koi8)
-             ("#altlinux" . cyrillic-koi8)
-             ("#unix.ru" . cyrillic-koi8)
-             ("#fidorus" . cyrillic-koi8)))
-     (setq erc-nick custom/erc-main-nick)
-     (setq erc-nick-uniquifier "_")
-     (setq erc-prompt-for-password nil)
-     (setq erc-kill-queries-on-quit t)
-     (setq erc-server-coding-system '(cp1251 . cp1251))
-
-     (defvar bitlbee-password "geheim")
-
-     (defun bitlbee-identify ()
-       "If we're on the bitlbee server, send the identify command to the &bitlbee channel."
-       (when (and (string= "localhost" erc-session-server)
-                  (string= "&bitlbee" (buffer-name)))
-         (erc-message "PRIVMSG" (format "%s identify %s"
-                                        (erc-default-target)
-                                        bitlbee-password))))
-     (add-hook 'erc-join-hook 'bitlbee-identify)
-
-     (defun erc-cmd-ICQWHOIS (uin)
-       "Queries icq-user with UIN `uin', and returns the result."
-       (let* ((result (myerc-query-icq-user uin))
-              (fname (cdr (assoc 'fname result)))
-              (lname (cdr (assoc 'lname result)))
-              (nick (cdr (assoc 'nick result))))
-         (erc-display-message nil 'notice (current-buffer) (format "%s (%s %s)" nick fname lname))))
-     ))
+(defun connect-office-irc ()
+  (interactive)
+  (erc-tls
+   :server private/erc-server-office
+   :full-name private/erc-full-name
+   :nick private/erc-nick-office
+   :password private/erc-password-office))
 
 (eval-after-load "twittering-mode"
   '(progn
@@ -90,6 +28,8 @@
      (global-set-key (kbd "C-c s") 'mingus)
      (define-key mingus-playlist-map (kbd "<Backspace>") 'mingus-del)
      ))
+
+(global-set-key (kbd "C-c C-i") 'connect-office-irc)
 
 (provide 'rc-clients)
 
