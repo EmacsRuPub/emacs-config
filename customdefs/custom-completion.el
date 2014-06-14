cc212f8663bf05d015292c2e75194de8a22b6e74
Alex <aaermolov@gmail.com>
2014-06-15 02:24:52 +0400

    rc: customdefs: complete yasnippet keys with ido

--- customdefs/custom-completion.el
+++ customdefs/custom-completion.el
@@ -99,6 +99,29 @@
   (when (string-match "/resources/yasnippet" buffer-file-name)
     (yas/load-directory custom/yasnippet-dir)))
 
+;; Completing point by some yasnippet key
+(defun yas-ido-expand ()
+  "Lets you select (and expand) a yasnippet key"
+  (interactive)
+  (let ((original-point (point)))
+    (while (and
+            (not (= (point) (point-min) ))
+            (not
+             (string-match "[[:space:]\n]" (char-to-string (char-before)))))
+      (backward-word 1))
+    (let* ((init-word (point))
+           (word (buffer-substring init-word original-point))
+           (list (yas-active-keys)))
+      (goto-char original-point)
+      (let ((key (remove-if-not
+                  (lambda (s) (string-match (concat "^" word) s)) list)))
+        (if (= (length key) 1)
+            (setq key (pop key))
+          (setq key (ido-completing-read "key: " list nil nil word)))
+        (delete-char (- init-word original-point))
+        (insert key)
+        (yas-expand)))))
+
 (provide 'custom-completion)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
