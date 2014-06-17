badb356bdfe6199a5c9e6c022e3186ae7bb2c7bc
Alex <aaermolov@gmail.com>
2014-06-18 03:01:39 +0400

    customdefs: orgmode: enhancements + refactoring

--- customdefs/custom-orgmode.el
+++ customdefs/custom-orgmode.el
@@ -8,15 +8,33 @@
 
 (defun save-last-scrum-timestamp ()
   (interactive)
+  (setq last-scrum-timestamp
+          (custom/strip-org-timestamp (custom/get-org-timestamp)))
+  (write-string-to-file
+   (elisp-var-printable 'last-scrum-timestamp)
+   (concat config-basedir "last-scrum-timestamp")))
+
+(defun custom/get-org-timestamp ()
   (with-temp-buffer
-    (call-interactively 'org-time-stamp)
-    (setq last-scrum-timestamp
-          (buffer-substring
-           (- (point-max) 4)
-           (1+ (point-min))))
-    (write-string-to-file
-     (elisp-var-printable 'last-scrum-timestamp)
-     (concat config-basedir "last-scrum-timestamp"))))
+    (call-interactively 'org-time-stamp-inactive)
+    (buffer-string)))
+
+(defun custom/strip-org-timestamp (ts)
+  (let ((tslen (length ts)))
+    (substring ts 1 (- tslen 4))))
+
+;; TODO maybe squash functions below some way
+(defun mark-with-finished-timestamp ()
+  (interactive)
+  (beginning-of-line)
+  (org-beginning-of-line)
+  (org-time-stamp-inactive)
+  (insert " "))
+
+(defun refile-job-done ()
+  (interactive)
+  (let ((org-refile-targets `((,(concat org-dir "/job/done.org") :level . 1))))
+    (call-interactively 'org-refile)))
 
 (provide 'custom-orgmode)
 
