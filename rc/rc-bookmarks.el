9b6379553ec11a1124ff220c5500860c90fbd190
Alex <aaermolov@gmail.com>
2014-06-30 18:50:34 +0400

    rc: bookmarks: updated bm options

--- rc/rc-bookmarks.el
+++ rc/rc-bookmarks.el
@@ -36,6 +36,8 @@
      (global-set-key (kbd "<M-f2>") 'bm-toggle)
      (global-set-key (kbd "<f2>")   'bm-next)
      (global-set-key (kbd "<S-f2>") 'bm-previous)
+     (global-set-key (kbd "<C-f2>") 'bm-show-all)
+     (setq bm-cycle-all-buffers t)
      ))
 
 (provide 'rc-bookmarks)
