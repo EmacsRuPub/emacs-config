a00efe77eb3e3cb3c07f4e4e9985a009af0eafd0
Alex <aaermolov@gmail.com>
2014-06-30 18:49:09 +0400

    rc: orgmode: quote -> "'"

--- rc/rc-org-mode.el
+++ rc/rc-org-mode.el
@@ -95,8 +95,7 @@
         ("u" alltodo ""
          ((org-agenda-skip-function
            (lambda nil
-             (org-agenda-skip-entry-if (quote scheduled) (quote deadline)
-                                       (quote regexp) "<[^>\n]+>")))
+             (org-agenda-skip-entry-if 'scheduled 'deadline 'regexp "<[^>\n]+>")))
           (org-agenda-overriding-header "Unscheduled TODO entries: ")))
         ("U" "Prioritized tasks"
          ((tags-todo "+PRIORITY=\"A\"")
