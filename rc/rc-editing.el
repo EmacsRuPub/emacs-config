db7d44c4c9a6b3c0a7d758a89eb869145d75e6e8
Alex <aaermolov@gmail.com>
2014-09-22 11:33:35 +0400

    rc: editing: updated evil-surround package
    now package inits itself directly in el-get recipe

--- rc/rc-editing.el
+++ rc/rc-editing.el
@@ -158,9 +158,7 @@
 (eval-after-load "evil"
   '(progn
      (require 'evil-nerd-commenter)
-     (require 'surround)
      (evilnc-default-hotkeys)
-     (global-surround-mode 1)
      (global-set-key (kbd "C-c v i") 'evil-mode)
      ))
 
