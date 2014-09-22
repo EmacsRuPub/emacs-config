5e224598a744a5d1af83878705c4dbe636e20148
Alex <aaermolov@gmail.com>
2014-09-22 09:23:30 +0400

    rc: lisp: removed annoying slime-highlight-edits minor mode

--- rc/rc-lisp.el
+++ rc/rc-lisp.el
@@ -31,7 +31,7 @@
 (eval-after-load "slime"
   '(progn
      (slime-setup
-      '(slime-fancy-inspector slime-fancy-trace slime-fontifying-fu slime-highlight-edits
+      '(slime-fancy-inspector slime-fancy-trace slime-fontifying-fu
         slime-hyperdoc slime-motd slime-package-fu slime-references
         slime-snapshot slime-sprof slime-trace-dialog slime-xref-browser slime-asdf slime-autodoc
         slime-banner slime-fancy slime-fuzzy slime-repl slime-sbcl-exts)) ;helm-slime
