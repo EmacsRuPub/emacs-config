757258d72e43ed10513099d161c81c55f02d5519
Alex <aaermolov@gmail.com>
2014-12-12 00:29:18 +0300

    customdefs: orgmode: extracted code for job notes and planning
    enhancements pending

--- customdefs/custom-orgmode.el
+++ customdefs/custom-orgmode.el
@@ -35,52 +35,6 @@
 (defun org-osd-display (min-to-app new-time msg)
   (osd-display msg msg -1 "center" "center" "Verdana 40"))
 
-(defvar last-scrum-timestamp nil)
-
-(defun save-last-scrum-timestamp ()
-  (interactive)
-  (setq last-scrum-timestamp
-          (custom/strip-org-timestamp (custom/get-org-timestamp)))
-  (write-string-to-file
-   (elisp-var-printable 'last-scrum-timestamp)
-   (at-config-basedir "last-scrum-timestamp")))
-
-(defun custom/get-org-timestamp ()
-  (with-temp-buffer
-    (call-interactively 'org-time-stamp-inactive)
-    (buffer-string)))
-
-(defun custom/strip-org-timestamp (ts)
-  (let ((tslen (length ts)))
-    (substring ts 1 (- tslen 4))))
-
-(defun custom/scrum-timestamp-as-tag ()
-  (replace-regexp-in-string
-   "[:-]" "_" (symbol-value 'last-scrum-timestamp)))
-
-(defun custom/insert-scrum-timestamp-tag ()
-  (interactive)
-  (insert (custom/scrum-timestamp-as-tag)))
-
-;; TODO maybe squash functions below some way
-(defun mark-with-finished-timestamp ()
-  (interactive)
-  (beginning-of-line)
-  (org-beginning-of-line)
-  (org-time-stamp-inactive)
-  (insert " "))
-
-(defun refile-job-done ()
-  (interactive)
-  (let ((org-refile-targets `((,(at-org-dir "/job/done.org") :level . 1))))
-    (call-interactively 'org-refile)))
-
-(defun process-task-done ()
-  (interactive)
-  (org-todo 'done)
-  (mark-with-finished-timestamp)
-  (refile-job-done))
-
 (defun open-or-browse-at-point (arg)
   (interactive "P")
   (if arg
