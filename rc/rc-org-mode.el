4fa191283065a5ba4b0a632caec51f75ae5df34e
Alex <aaermolov@gmail.com>
2014-12-18 14:44:15 +0300

    rc: orgmode: system: fixed paths to org bundle

--- rc/rc-org-mode.el
+++ rc/rc-org-mode.el
@@ -26,8 +26,7 @@
 ;############################################################################
 (add-to-list 'file-coding-system-alist (cons "\\.\\(org\\|org_archive\\|/TODO\\)$"  'utf-8))
 
-(setq org-agenda-files (append (all-files-under-dir-recursively (at-org-dir) "org")
-                               (all-files-under-dir-recursively (at-org-mastering-dir) "org")))
+(setq org-agenda-files (append (all-files-under-dir-recursively (at-org-dir) "org")))
 ;TODO: maybe do it less straightforward
 (add-to-list 'org-agenda-files (at-config-basedir "todo.org"))
 (add-to-list 'org-agenda-files (at-config-basedir "totry.org"))
--- system/pathutil.el
+++ system/pathutil.el
@@ -7,13 +7,4 @@
   (concat home-directory suffix))
 
 (defun at-org-dir (&optional suffix)
-  (concat (at-homedir "/org/main") suffix))
-
-(defun at-org-inventory-dir (&optional suffix)
-  (concat (at-homedir "/org/inventory") suffix))
-
-(defun at-org-mastering-dir (&optional suffix)
-  (concat (at-homedir "/org/mastering") suffix))
-
-(defun at-org-job-dir (&optional suffix)
-  (concat (at-homedir "/org/job") suffix))
+  (concat (at-homedir "/org") suffix))
