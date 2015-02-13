07ed5cb8ed81fffec939e3e309101bd751ca0e8b
Alex Ermolov <aermolov@mirantis.com>
2015-02-13 20:11:13 +0300

    el-get: removed obsolete custom recipes

--- el-get-custom-recipes/prodigy.el
+++
@@ -1,6 +0,0 @@
-(:name prodigy
-       :description "Manage external services from within Emacs"
-       :type github
-       :pkgname "purcell/prodigy.el"
-       :depends ht
-       :load-path ".")
