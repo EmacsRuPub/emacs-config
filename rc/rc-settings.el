10ca00755a669d0d4c093e0b1ba8dfad29f5c749
Alex Ermolov <aermolov@mirantis.com>
2015-02-15 21:36:36 +0300

    rc: settings: reorganized source

--- rc/rc-settings.el
+++ rc/rc-settings.el
@@ -4,47 +4,23 @@
 ;; Created:  Fri May 23 23:08:51 2014 +0400
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;;; Enable functions that are disabled by default
-
-;; Upcase and downcase regions
-(put 'upcase-region 'disabled nil)
-(put 'downcase-region 'disabled nil)
-
-;; Sets the current horizontal position for C-n and C-p
-(put 'set-goal-column 'disabled nil)
-
-;; Restrict buffer editing to a region
-(put 'narrow-to-region 'disabled nil)
-
-;; Dired functions
-(put 'dired-find-alternate-file 'disabled nil)
-
-;;{{{ Horizontal scrolling
-(put 'scroll-left 'disabled nil)
-(put 'scroll-right 'disabled nil)
-;;}}}
-
-;; some useful funcs
-(put 'erase-buffer 'disabled nil)
-(put 'upcase-region 'disabled nil)
-(put 'downcase-region 'disabled nil)
-
-;; encoding settings
-(prefer-coding-system 'utf-8)
-(set-buffer-file-coding-system 'utf-8 'utf-8-unix)
-(set-default buffer-file-coding-system 'utf-8-unix)
-(set-default default-buffer-file-coding-system 'utf-8-unix)
-(set-default-coding-systems 'utf-8)
-(set-keyboard-coding-system 'utf-8)
-(set-selection-coding-system 'utf-8)
-(set-terminal-coding-system 'utf-8)
-(setq locale-coding-system 'utf-8)
-
 (load-library "time")
 
 (require 'uniquify)
 (require 'popwin)
 (require 'tail)
+
+;;; Enable functions that are disabled by default
+(put 'upcase-region 'disabled nil)
+(put 'downcase-region 'disabled nil)
+(put 'set-goal-column 'disabled nil)
+(put 'narrow-to-region 'disabled nil)
+(put 'dired-find-alternate-file 'disabled nil)
+(put 'scroll-left 'disabled nil)
+(put 'scroll-right 'disabled nil)
+(put 'erase-buffer 'disabled nil)
+(put 'upcase-region 'disabled nil)
+(put 'downcase-region 'disabled nil)
 
 (setq size-indication-mode t)
 (setq column-number-mode t)
@@ -61,33 +37,42 @@
 (setq visible-bell t)
 (setq inhibit-startup-echo-area-message "octocat")
 (setq eval-expression-print-length nil)
-
-(setq echo-keystrokes 0.1);; Show keystrokes in progress
-
-;; Non-customizable variables
+(setq echo-keystrokes 0.1)
 (setq gc-cons-percentage 0.3)
 (setq print-gensym t)
 (setq print-circle t)
 (setq redisplay-dont-pause t)
-
-;; date|time
 (setq display-time-mail-file t)
 (setq display-time-form-list (list 'time 'load))
 (setq display-time-day-and-date t)
 (setq display-time-string-forms
   '( day " " monthname " (" dayname ") " 24-hours ":" minutes))
-
 (setq disabled-command-function nil)
-(and (fboundp 'scroll-bar-mode) (scroll-bar-mode 0))
-
-;; uniquify
 (setq uniquify-buffer-name-style 'post-forward)
 (setq uniquify-separator "/")
-(setq uniquify-ignore-buffers-re "^\\*") ; don't muck with special buffers
+(setq uniquify-ignore-buffers-re "^\\*")
 (setq uniquify-strip-common-suffix nil)
-
 (setq tail-max-size 20)
+(setq font-lock-maximum-decoration t)
+(setq color-theme-is-global t)
+(setq truncate-partial-width-windows nil)
+(setq backup-by-copying t)
+(setq backup-by-copying-when-linked t)
+(setq backup-directory-alist `(("." . ,emacs-tmp-dir)))
+(setq auto-save-file-name-transforms `((".*" ,emacs-tmp-dir t)))
+(setq delete-old-versions -1)
+(setq version-control t)
 
+(and (fboundp 'scroll-bar-mode) (scroll-bar-mode 0))
+(prefer-coding-system 'utf-8)
+(set-buffer-file-coding-system 'utf-8 'utf-8-unix)
+(set-default buffer-file-coding-system 'utf-8-unix)
+(set-default default-buffer-file-coding-system 'utf-8-unix)
+(set-default-coding-systems 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(set-selection-coding-system 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(setq locale-coding-system 'utf-8)
 (menu-bar-mode -1)
 (scroll-bar-mode 0)
 (blink-cursor-mode 0)
@@ -96,16 +81,8 @@
 (tooltip-mode nil)
 (global-font-lock-mode t)
 (display-battery-mode 1)
-
-;; use y/n instead of yes/no
-(fset 'yes-or-no-p 'y-or-n-p)
-
-;;часы в статусе
 (display-time)
-
-(setq font-lock-maximum-decoration t)
-(setq color-theme-is-global t)
-(setq truncate-partial-width-windows nil)
+(fset 'yes-or-no-p 'y-or-n-p)
 
 (defun custom/after-init-hook ()
   (with-eval-after-load "diminish"
@@ -120,13 +97,6 @@
 (add-hook 'after-init-hook 'custom/after-init-hook)
 
 (global-set-key (kbd "C-c t f") 'tail-file)
-
-(setq backup-by-copying t)              ; don't clobber symlinks
-(setq backup-by-copying-when-linked t)  ; Copy linked files, don't rename.
-(setq backup-directory-alist `(("." . ,emacs-tmp-dir)))    ; don't litter my fs tree
-(setq auto-save-file-name-transforms `((".*" ,emacs-tmp-dir t)))
-(setq delete-old-versions -1)
-(setq version-control t)               ; use versioned backups
 
 (provide 'rc-settings)
 
