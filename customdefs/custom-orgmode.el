03161d7b9016bef7af3e2e5fb40bc72d277b8d25
Alex Ermolov <aermolov@mirantis.com>
2015-04-06 15:40:52 +0300

    customdefs: orgmode: more searchable function name

--- customdefs/custom-orgmode.el
+++ customdefs/custom-orgmode.el
@@ -50,7 +50,7 @@
 With prefix argument, also display headlines without a TODO keyword."
   (org-tags-view nil tag))              ;nil was (null current-prefix-arg) originally
 
-(defun update-timestamp-with-time (&optional as-is)
+(defun custom/org-update-timestamp-with-time (&optional as-is)
   (interactive)
   (unless as-is
     (insert " "))
