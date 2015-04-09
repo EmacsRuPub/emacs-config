b84d3509906015c4ac92dc568bf022fbe09d0124
Alex Ermolov <aermolov@mirantis.com>
2015-04-09 12:44:45 +0300

    rc: python: (re)indenting source using region-bindings-mode

--- rc/rc-python.el
+++ rc/rc-python.el
@@ -76,6 +76,8 @@
     (bind-key "C-c r" 'helm-jedi-related-names python-mode-map) ;NOTE: just to memory the existence for the first time
     (bind-key "C-M-." 'python-indent-shift-right python-mode-map)
     (bind-key "C-M-," 'python-indent-shift-left python-mode-map)
+    (bind-key "+" 'python-indent-shift-right region-bindings-mode-map)
+    (bind-key "_" 'python-indent-shift-left region-bindings-mode-map)
     ))
 
 (setq mumamo-background-colors nil)
