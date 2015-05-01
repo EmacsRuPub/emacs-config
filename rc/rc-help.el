2e047c5ba06cb686232c0c55fb9ea6a8d0c2d187
Alex Ermolov <aermolov@mirantis.com>
2015-05-01 19:11:04 +0300

    rc: help: apropos/Info enhancements

--- rc/rc-help.el
+++ rc/rc-help.el
@@ -10,7 +10,39 @@
 (push "/usr/share/info" Info-directory-list)
 (setenv "MANPATH" "/usr/share/man:/usr/local/man")
 
-(global-set-key [f1] 'help-on-click/key)
+(defun custom/open-info (topic bname)
+  "Open info on TOPIC in BNAME."
+  (if (get-buffer bname)
+      (progn
+        (switch-to-buffer bname)
+        (unless (string-match topic Info-current-file)
+          (Info-goto-node (format "(%s)" topic))))
+    (info topic bname)))
+
+(setq Info-additional-directory-list
+      (list (concat home-directory "/help/info")))
+
+(defhydra hydra-apropos (global-map "C-c h"
+                         :color blue
+                         :hint nil)
+  "Apropos"
+  ("a" apropos)
+  ("d" apropos-documentation)
+  ("v" apropos-variable)
+  ("c" apropos-command)
+  ("l" apropos-library)
+  ("u" apropos-user-option)
+  ("e" apropos-value))
+
+;TODO: bind to key
+(defhydra hydra-info-to (global-map "C-c C-h"
+                         :hint nil :color teal)
+  "
+_o_rg e_l_isp _e_macs _h_yperspec"
+  ("o" (custom/open-info "org" "*org info*"))
+  ("e" (custom/open-info "elisp" "*elisp info*"))
+  ("m" (custom/open-info "emacs" "*emacs info*"))
+  ("h" (custom/open-info "gcl" "*hyperspec*")))
 
 (provide 'rc-help)
 
