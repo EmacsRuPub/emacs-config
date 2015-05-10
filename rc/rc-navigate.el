f79c053af8d4e7246847c41f7422b98cc8a8cad2
Alex Ermolov <aermolov@mirantis.com>
2015-05-10 22:57:12 +0300

    rc: navigate: new hydra for breadcrumb package

--- rc/rc-navigate.el
+++ rc/rc-navigate.el
@@ -115,9 +115,22 @@
 
 (use-package breadcrumb
   :config
-  (progn
-    (bind-key (kbd "j") 'bc-list custom-search-keymap)
-    (bind-key (kbd "e") 'bc-set custom-search-keymap)))
+  (defhydra hydra-breadcrumb (:exit t)
+    "
+Breadcrumb bookmarks:
+  _<up>_:   prev   _S-<up>_:   local prev
+  _<down>_: next   _S-<down>_: local next
+  _s_: set  _c_: clear  _l_: list  _q_: quit
+"
+    ("<down>" bc-next nil :exit nil)
+    ("<up>" bc-previous nil :exit nil)
+    ("S-<down>" bc-local-next nil :exit nil)
+    ("S-<up>" bc-local-previous nil :exit nil)
+    ("l" bc-list nil)
+    ("s" bc-set nil)
+    ("c" bc-clear nil)
+    ("q" nil nil)))
+(global-set-key (kbd "<f12>") 'hydra-breadcrumb/body)
 
 (use-package zoom-window
   :bind ("C-x C-z" . zoom-window-zoom)
