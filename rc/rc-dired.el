0d3788e3ac840cda3d62ab2dfc5ab3cb0700dc08
Alex Ermolov <aermolov@mirantis.com>
2015-05-20 13:28:22 +0300

    rc: dired: "use-package" everywhere

--- rc/rc-dired.el
+++ rc/rc-dired.el
@@ -4,19 +4,31 @@
 ;; Created:  Fri May 24 22:41:54 2013 +0400
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(use-package dired)
-(use-package dired+)
-(use-package dired-x)
-(use-package dired-toggle-sudo)
+(use-package dired
+  :config
+  (setq dired-recursive-deletes 'top) ;; Allows recursive deletes
+  (setq dired-dwim-target t)
+  (setq dired-listing-switches "-lah1v --group-directories-first")
+  (global-set-key (kbd "C-c x") 'direx:jump-to-directory)
+  ;; (global-set-key (kbd "C-c C-j") 'dired-jump)
+  (bind-key "C-c C-s" 'dired-toggle-sudo dired-mode-map)
+  (bind-key "C-c C-m" 'custom/get-file-md5 dired-mode-map)
+  (bind-key "!" 'custom/sudo-dired dired-mode-map)
+  (bind-key "C-a" 'custom/dired-back-to-start-of-files dired-mode-map)
+  (bind-key "C-x C-k" 'dired-do-delete dired-mode-map) ;; Delete with C-x C-k to match file buffers and magit
+  (bind-key "`" 'custom/dired-open-term dired-mode-map)
+  (define-key dired-mode-map (vector 'remap 'beginning-of-buffer) 'custom/dired-back-to-top)
+  (define-key dired-mode-map (vector 'remap 'smart-up) 'custom/dired-back-to-top)
+  (define-key dired-mode-map (vector 'remap 'end-of-buffer) 'custom/dired-jump-to-bottom)
+  (define-key dired-mode-map (vector 'remap 'smart-down) 'custom/dired-jump-to-bottom))
 
-;; TODO: check if this is not obsolete yet
-(setq diredp-ignored-file-name 'green-face)
-(setq diredp-other-priv 'white-face)
-(setq diredp-rare-priv 'white-red-face)
-(setq diredp-compressed-file-suffix 'darkyellow-face)
-(setq dired-recursive-deletes 'top) ;; Allows recursive deletes
-(setq dired-dwim-target t)
-(setq dired-listing-switches "-lah1v --group-directories-first")
+(use-package dired+
+  :config
+  ;; TODO: check if this is not obsolete yet
+  (setq diredp-ignored-file-name 'green-face)
+  (setq diredp-other-priv 'white-face)
+  (setq diredp-rare-priv 'white-red-face)
+  (setq diredp-compressed-file-suffix 'darkyellow-face))
 
 (use-package wdired
   :defer t
@@ -30,25 +42,15 @@
     (bind-key "r" 'wdired-change-to-wdired-mode dired-mode-map)
     ))
 
+(use-package dired-x)
+(use-package dired-toggle-sudo)
+
 ;; Reload dired after making changes
 (--each '(dired-do-rename
           dired-create-directory
           wdired-abort-changes)
         (eval `(defadvice ,it (after revert-buffer activate)
                  (revert-buffer))))
-
-(global-set-key (kbd "C-c x") 'direx:jump-to-directory)
-(define-key dired-mode-map (kbd "C-c C-s") 'dired-toggle-sudo)
-(define-key dired-mode-map (kbd "C-c C-m") 'custom/get-file-md5)
-(define-key dired-mode-map (kbd "!") 'custom/sudo-dired)
-;; (global-set-key (kbd "C-c C-j") 'dired-jump)
-(define-key dired-mode-map (kbd "C-a") 'custom/dired-back-to-start-of-files)
-(define-key dired-mode-map (vector 'remap 'beginning-of-buffer) 'custom/dired-back-to-top)
-(define-key dired-mode-map (vector 'remap 'smart-up) 'custom/dired-back-to-top)
-(define-key dired-mode-map (vector 'remap 'end-of-buffer) 'custom/dired-jump-to-bottom)
-(define-key dired-mode-map (vector 'remap 'smart-down) 'custom/dired-jump-to-bottom)
-(define-key dired-mode-map (kbd "C-x C-k") 'dired-do-delete) ;; Delete with C-x C-k to match file buffers and magit
-(define-key dired-mode-map (kbd "`") 'custom/dired-open-term)
 
 (provide 'rc-dired)
 
