fff69c5c7728ce5f5e6c2591d3cad3157f44c215
Alex Ermolov <aermolov@mirantis.com>
2015-09-07 11:53:27 +0300

    customdefs: rc: programming: relocated postgresql c-mode definition

--- rc/rc-programming.el
+++ rc/rc-programming.el
@@ -156,6 +156,16 @@ PROJECT: %(projectile-project-root)
     (setq ediff-split-window-function 'split-window-horizontally)
     (setq ediff-diff-options "-w")))
 
+(defun pgsql-c-mode ()
+  ;; sets up formatting for PostgreSQL C code
+  (interactive)
+  (c-mode)
+  (setq-default tab-width 4)
+  (c-set-style "bsd")             ; set c-basic-offset to 4, plus other stuff
+  (c-set-offset 'case-label '+)   ; tweak case indent to match PG custom
+  (setq fill-column 79)           ; matches what pgindent does
+  (setq indent-tabs-mode t))      ; make sure we keep tabs when indenting
+
 (provide 'rc-programming)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
