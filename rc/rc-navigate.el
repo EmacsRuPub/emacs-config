3643353d21037b2d0ec5717b2b734fbd842b5a23
Alex Ermolov <aermolov@mirantis.com>
2015-11-08 00:08:54 +0300

    rc: navigate: removed keybinding clash for self-inserting asterisk sign

--- rc/rc-navigate.el
+++ rc/rc-navigate.el
@@ -53,7 +53,7 @@
     (bind-key "C-<down>" 'helm-next-source helm-map)
     (bind-key "C-<up>" 'helm-previous-source helm-map)
     (bind-key "C-x b" 'helm-buffers-list)
-    (bind-key "*" 'helm-mark-all helm-map)
+    (bind-key "C-*" 'helm-mark-all helm-map)
     (bind-key "C-x j j" 'helm-bookmarks)
     (bind-key "M-x" 'helm-M-x)
     (bind-key "C-h a" 'helm-apropos)
