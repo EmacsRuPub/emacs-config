161b3f20b7c73a4f00f401f7b944dfc6ece07753
Alex Ermolov <aermolov@mirantis.com>
2015-11-21 12:51:57 +0300

    moved package.el machinery back to init.el
    otherwise it goes the following way:
    config is begin tangled with builtin Org version, and then
    the one from package.el gets loaded over it. Therefore the org
    customizations get broken in some non-straightforward way.

--- init.el
+++ init.el
@@ -4,4 +4,36 @@
 (defun at-config-basedir (&optional suffix)
   (concat config-basedir suffix))
 
+(require 'package)
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.milkbox.net/packages/"))
+(add-to-list 'package-archives
+             '("elpa" . "http://tromey.com/elpa/"))
+(add-to-list 'package-archives
+             '("org" . "http://orgmode.org/elpa/"))
+(package-initialize)
+
+(mapcar
+ (lambda (package)
+   (unless (package-installed-p package)
+     (package-install package)))
+ ;;base system packages for bootstrapping
+ '(bind-key diminish use-package f names org))
+
+(require 'use-package)
+(setq use-package-verbose t)
+(setq load-prefer-newer t)
+
+(use-package auto-compile
+  :ensure t
+  :config
+  (auto-compile-on-load-mode 1)
+  (auto-compile-on-save-mode 1)
+  (setq auto-compile-display-buffer nil)
+  (setq auto-compile-mode-line-counter t))
+
+(use-package f :ensure t)
+
 (org-babel-load-file (at-config-basedir "config.org"))
