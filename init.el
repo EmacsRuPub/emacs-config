c9388d9099228eeb448a09d2aa78296a49177a14
Alex Ermolov <aermolov@mirantis.com>
2016-01-28 17:26:53 +0300

    further simplified init.el

--- init.el
+++ init.el
@@ -1,9 +1,3 @@
-(setq config-basedir (file-name-directory
-                      (or (buffer-file-name) load-file-name)))
-
-(defun at-config-basedir (&optional suffix)
-  (concat config-basedir suffix))
-
 (require 'package)
 (add-to-list 'package-archives
              '("marmalade" . "https://marmalade-repo.org/packages/"))
@@ -15,6 +9,9 @@
       "https://elpa.gnu.org/packages/")
 (package-initialize)
 
+(setq use-package-verbose t)
+(setq load-prefer-newer t)
+
 (mapcar
  (lambda (package)
    (unless (package-installed-p package)
@@ -23,26 +20,12 @@
  '(bind-key use-package))
 
 (require 'use-package)
-(setq use-package-verbose t)
-(setq load-prefer-newer t)
 
-(use-package auto-compile
-  :ensure t
-  :config
-  (auto-compile-on-load-mode 1)
-  (auto-compile-on-save-mode 1)
-  (setq auto-compile-display-buffer nil)
-  (setq auto-compile-mode-line-counter t))
-
-(use-package f :ensure t)
-(use-package diminish :ensure t)
-(use-package names :ensure t)
 (use-package org-plus-contrib :ensure t)
 (setq org-modules
       '(org-agenda org-archive org-attach org-bbdb org-bibtex org-bookmark
         org-checklist org-clock org-collector org-docview org-drill org-expiry
         org-habit org-id org-info org-inlinetask org-interactive-query org-irc
         org-man org-mouse org-protocol org-timer org-w3m))
-(use-package anaphora :ensure t)
 
-(org-babel-load-file (at-config-basedir "config.org"))
+(org-babel-load-file (concat (file-name-directory load-file-name) "config.org"))
